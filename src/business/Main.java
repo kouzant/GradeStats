8ab1d2ac54c3c116069f9b2ad4dedd55fcbc6270
alafroiskiotos <praetorian@akyroi.gr>
2011-07-28 21:44:32 +0300

    I have grades and counts

--- src/business/Main.java
+++ src/business/Main.java
@@ -1,14 +1,33 @@
 package business;
 
-import java.util.LinkedList;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedList;
 
 public class Main {
 
 	public static void main(String[] args) {
+		int pass = 0;
+		int fail = 0;
 		ScrapeGrades sg = new ScrapeGrades(args[0]);
 		LinkedList<Integer> grades = sg.scrape();
 		System.out.println(grades);
+		HashMap<Integer, Integer> exp = new HashMap<Integer, Integer>();
+		for(int i = 0; i <= 10; i++)
+			exp.put(i, 0);
+		Iterator<Integer> gradesIt = grades.iterator();
+		while(gradesIt.hasNext()){
+			int tmpGrade = gradesIt.next();
+			if(tmpGrade >= 5)
+				pass++;
+			else
+				fail++;
+			int curCount = exp.get(tmpGrade);
+			exp.put(tmpGrade, ++curCount);
+		}
+		System.out.println(exp);
+		System.out.println("Pass: "+pass);
+		System.out.println("Fail: "+fail);
 	}
 
 }
